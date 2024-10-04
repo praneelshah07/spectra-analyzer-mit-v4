e4f831f604730cda4a08400d0deeb9ebecad5c7a
praneelshah07 <praneelshah101@gmail.com>
2024-10-03 19:41:11 -0700

    random again?

--- streamlit_app.py
+++ streamlit_app.py
@@ -390,9 +390,10 @@ with main_col2:
                             x_axis = wavelength
                         ax.fill_between(x_axis, 0, spectra, color="k", alpha=0.01)
 
-                for smiles in target_spectra:
+                for i, smiles in enumerate(target_spectra):
                     spectra = target_spectra[smiles]
-                    ax.fill_between(x_axis, 0, spectra, color=color_options[i % len(color_options)], alpha=0.5, label=f"{smiles}")
+                    ax.fill_between(x_axis, 0, spectra, color=color_options[i % len(color_options)], 
+                    alpha=0.5, label=f"{smiles}")
 
                     if peak_finding_enabled:
                         # Detect peaks and retrieve peak properties like prominence
